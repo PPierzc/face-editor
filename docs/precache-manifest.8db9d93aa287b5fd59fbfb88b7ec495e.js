e0dabe609c1bacdeb80a75c4652d16cc1a949921
Paweł <ppierzc@gmail.com>
2019-05-02 15:14:16 +0200

    [BUILD] renamed to docs

---
+++ docs/precache-manifest.8db9d93aa287b5fd59fbfb88b7ec495e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "77dc0f18a34eeb1512078d1835cbb73f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "912547bfa1c50cc985f5",
+    "url": "/static/css/main.584f321a.chunk.css"
+  },
+  {
+    "revision": "f36df06c987c9e3fa880",
+    "url": "/static/js/2.c4357c67.chunk.js"
+  },
+  {
+    "revision": "912547bfa1c50cc985f5",
+    "url": "/static/js/main.ef40af4c.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
